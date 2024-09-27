c693c4e421b003f4685622566b79f780036dc9da
Aadit <mail2aaditsarhadi@gmail.com>
2024-09-27 22:24:22 +0530

    Update variables.py

--- variables.py
+++ variables.py
@@ -1,4 +1,26 @@
 # Variable = A container for a value( string, integer, float, boolean)
 
-#
+'''Rules for declaring variable 
+It must start with a letter or underscore.
+It cannot start with a number.
+Variables are case-sensitive.
+It can only contain alpha-numeric characters and only one special character i.e underscore (_).
+A keyword can't be used as variable.'''
+
+# Strings
 name = "Aadit"
+print("Hello", name)
+hobby= "coding"
+print(f"I like {hobby}") # Printing using f-string
+
+# Integer
+age = 17
+print(f"I am {age} years old")
+
+# Float
+cgpa = 9.7
+distance = 10.55
+
+# Boolean 
+True => 1
+False => 0
