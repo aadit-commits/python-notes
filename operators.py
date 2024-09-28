2f5a7de03d1ec024b11b6ddb6c35f1de0b6b59c6
Aadit <mail2aaditsarhadi@gmail.com>
2024-09-28 18:10:48 +0530

    Create operators.py

---
+++ operators.py
@@ -0,0 +1,8 @@
+# Arithematic operators -> ( +, -, *, /, //, % )
+
+print(a+b) # Prints the sum 
+print(a-b) # Prints the difference
+print(a*b) # Prints the product
+print(a/b) # Divides a & b
+print(a//b) # Floor Division : Divides and returns the value in int datatype
+print(a%b) # Modulo Operator : Prints the remainder
