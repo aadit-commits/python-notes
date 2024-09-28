e86372b5451e708ccb3015672144f52aed966787
Aadit <mail2aaditsarhadi@gmail.com>
2024-09-28 17:48:07 +0530

    Update typecasting.py

--- typecasting.py
+++ typecasting.py
@@ -1,1 +1,7 @@
 #Typecasting = The process of converting a variable from one data type to another ( str(), int(), float(), bool() )
+
+a=15
+type(a) # Returns the dataype of variable a i.e "int"
+str(a) # Converts integer dataype into string
+bool(a) # Converts integer datatype into boolean
+float(a) # Converts integer dataype into float
