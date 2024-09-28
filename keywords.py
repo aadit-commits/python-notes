71a89bdb1e9a5dcc1bede99e1cc62132a4ad0c11
Aadit <mail2aaditsarhadi@gmail.com>
2024-09-28 18:14:53 +0530

    Create keywords.py

---
+++ keywords.py
@@ -0,0 +1,8 @@
+# Keywords are the reserved words in python which have their own meaning, there are total of 36 keywords in python as of now 
+
+The list of keywords are: 
+['False', 'None', 'True',"__peg_parser__", 'and', 'as', 'assert', 'async', 'await', 'break', 
+'class', 'continue', 'def', 'del', 'elif', 'else', 'except', 'finally', 'for', 'from', 'global', 'if', 
+'import', 'in', 'is', 'lambda', 'nonlocal', 'not', 'or', 'pass', 'raise', 'return', 'try', 'while', 'with', 'yield']
+
+# All keywords are in lowercase except 3 i.e True, False and None
