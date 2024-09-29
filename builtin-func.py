00ad4dd757f41dcba7ddcd0fc7382a0a199e0ae2
Aadit <mail2aaditsarhadi@gmail.com>
2024-09-29 17:48:34 +0530

    Update builtin-func.py

--- builtin-func.py
+++ builtin-func.py
@@ -1,1 +1,9 @@
+x = 3.14
+y = 3
+z = 2
 
+print(round(x)) # Prints the value after rounding it off
+print(abs(y)) # Changes the sign of the number and prints its absolute value
+print(pow(y,z)) # Prints y to the power of z
+print(max(x,y,z)) # Prints the maximum number
+print(min(x,y,z)) # Prints the minimum number
