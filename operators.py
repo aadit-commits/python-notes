83abcb1dc2e9dcc930d69474b76cd13029a4956c
Aadit <mail2aaditsarhadi@gmail.com>
2024-10-03 18:39:23 +0530

    Update operators.py

--- operators.py
+++ operators.py
@@ -6,3 +6,8 @@ print(a*b) # Prints the product
 print(a/b) # Divides a & b
 print(a//b) # Floor Division : Divides and returns the value in int datatype
 print(a%b) # Modulo Operator : Prints the remainder
+
+# Logical operators -> ( and, or , not)
+# or = at least one condition must be true
+# and = both conditions must be true
+# not = inverts the condition
