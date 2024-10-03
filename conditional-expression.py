edbf10efc7702d8fcfdf2bd4794268cde82a8b99
Aadit <mail2aaditsarhadi@gmail.com>
2024-10-03 22:42:38 +0530

    Create conditional-expression.py

---
+++ conditional-expression.py
@@ -0,0 +1,8 @@
+'''
+conditional expression = A one-line shortcut for the if-else statement (ternary operator)
+Print or assign one of two values based on a condition
+X if condition else Y
+'''
+
+num=5
+print("Positive" if num>0 else "Negative")
