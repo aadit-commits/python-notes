67155b587b9f83cd41375b27836e31ecb77c05a4
Aadit <mail2aaditsarhadi@gmail.com>
2024-10-03 17:46:35 +0530

    Update precedence.py

--- precedence.py
+++ precedence.py
@@ -1,4 +1,5 @@
 '''
 For order of precedence in Python we use the PEDMAS rules just like (BODMAS), i.e
 Parentheses > Exponential > Division > Multiplication > Addition > Subtraction
+Operators with the same precedence are evaluated left to right.
 '''
